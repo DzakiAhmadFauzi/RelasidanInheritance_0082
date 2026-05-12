6c4938608bb8aa6047efadc9ba41a9f6d3839aba
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2026-05-12 20:44:15 +0700

    commit 16

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -36,3 +36,10 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { //auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
